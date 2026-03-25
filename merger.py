43c80839a53785818dc577caba6165d2b3875ad0
Marcos Corazza <mcs.corazza@gmail.com>
2026-03-25 08:39:38 -0300

    fix: add calc critical to merger

--- merger.py
+++ merger.py
@@ -24,6 +24,7 @@ def process_data(batch_id, json_files, process_type, chunk_size):
       
       finish_pos = get_pos(json_files[-1])
       process_json_files(batch_id, json_files)
+      trip_critical_analisis(batch_id)
       update_trip_state(batch_id=batch_id, start_pos=start_pos, current_pos=finish_pos, is_finished=True)
 
     case 3:
@@ -32,7 +33,6 @@ def process_data(batch_id, json_files, process_type, chunk_size):
         start_pos = get_pos(json_files[0])
       else:
         start_pos = None
-      
       finish_pos = get_pos(json_files[-1])
       process_json_files(batch_id, json_files)
       trip_critical_analisis(batch_id)
@@ -44,10 +44,10 @@ def process_data(batch_id, json_files, process_type, chunk_size):
         start_pos = get_pos(json_files[0])
       else:
         start_pos = None
-        
       finish_pos = get_pos(json_files[-1])
       large_process_data(batch_id, json_files, chunk_size, is_finish=False)
       update_trip_state(batch_id=batch_id, current_pos=finish_pos, is_finished=False)
+      trip_critical_analisis(batch_id)
 
     case 5:
       print("\n    ✅ Complete large trip with FINISH. Process chunks.")
@@ -59,6 +59,8 @@ def process_data(batch_id, json_files, process_type, chunk_size):
       finish_pos = get_pos(json_files[-1])
       large_process_data(batch_id, json_files, chunk_size, is_finish=True)
       update_trip_state(batch_id=batch_id, current_pos=finish_pos, is_finished=True)
+      trip_critical_analisis(batch_id)
+
     case _:
       print("\n    ❌ Error!")
 
